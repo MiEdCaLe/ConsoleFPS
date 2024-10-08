f42cc69279d5a5f686b586fde14da717bf1f0514
MiEdCaLe <miguel.e.castillo.leyva@gmail.com>
2024-10-07 23:53:51 -0700

    added movement, shading

--- ConsoleFPS.cpp
+++ ConsoleFPS.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <iostream>
+#include <chrono>
 
 int nScreenWidth = 120;
 int nScreenHeight = 40;
@@ -17,7 +18,7 @@ float fDepth = 16.0f;
 int main()
 {
 	// Create Screen Buffer
-	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight]; //2 Dimensional Array 
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight]; //1 Dimensional Array 
 
 	// Create handle to console
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL , CONSOLE_TEXTMODE_BUFFER, NULL);
@@ -29,23 +30,60 @@ int main()
 	// We store the map in this string
 	map += L"################";
 	map += L"#..............#";
+	map += L"#....#######...#";
 	map += L"#..............#";
 	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
+	map += L"#..#...........#";
+	map += L"#..#...........#";
+	map += L"#..#...........#";
+	map += L"#..#...........#";
+	map += L"#..#...........#";
+	map += L"#..#...........#";
+	map += L"#..#...........#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"#..............#";
 	map += L"################";
+	
+	// set time point 1  to current time and define time point 2
+	std::chrono::time_point<std::chrono::system_clock> tp1 = std::chrono::system_clock::now();
+	std::chrono::time_point<std::chrono::system_clock> tp2;
 
 	while (1) {
+		
+		// Clock that measures the time that every frame takes
+		// Set time point 2 to current time.
+		tp2 = std::chrono::system_clock::now();
 
+		// Calculate the elapsed time between tp2 and tp1
+		std::chrono::duration<float> elapsedTime = tp2 - tp1;
+
+		// update the old time point.
+		tp1 = tp2;
+
+		float fElapsedTime = elapsedTime.count();
+
+		// Controls
+		// Handle CCW Rotation
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
+			fPlayerA -= (1.5) * fElapsedTime;
+
+		// Handle Clockwise Rotation
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
+			fPlayerA += (1.5) * fElapsedTime;
+
+		// Handle forward movement
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000) {
+			fPlayerX += cosf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerY += sinf(fPlayerA) * 5.0f * fElapsedTime;
+		}
+			
+		// Handle backward movement
+		if (GetAsyncKeyState((unsigned short)'S') & 0x8000) {
+			fPlayerX -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+			fPlayerY -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+		}
+	
 		for (int x{ 0 }; x < nScreenWidth; x++) {
 
 			// For each column, calculate the projected ray angle into world space
@@ -55,8 +93,8 @@ int main()
 			bool bHitWall{ false };
 
 			// We decompose the RayAngle unit vector into it's respective X and Y components. 
-			float fEyeX = sinf(fRayAngle);
-			float fEyeY = cosf(fRayAngle);
+			float fEyeX = cosf(fRayAngle);
+			float fEyeY = sinf(fRayAngle);
 
 			while (!bHitWall && fDistanceToWall < fDepth) {
 				
@@ -84,15 +122,31 @@ int main()
 			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
 			int nFloor = nScreenHeight - nCeiling;
 
+			short nShade = ' ';
+
+			if (fDistanceToWall <= fDepth / 4.0f) nShade = 0x2588; // Very Close
+			else if (fDistanceToWall < fDepth / 3.0f) nShade = 0x2593;
+			else if (fDistanceToWall < fDepth /2.0f) nShade = 0x2592;
+			else if (fDistanceToWall < fDepth) nShade = 0x2591;
+			else nShade = ' '; // Too Far Away
 
 			for (int y = 0; y < nScreenHeight; y++) {
-				if (y < nCeiling) {
+				// Shade ceiling
+				if (y <= nCeiling) {
 					screen[y * nScreenWidth + x] = ' ';
 				}
+				// Shade Walls
 				else if(y > nCeiling && y <= nFloor) {
-					screen[y * nScreenWidth + x] = '#';
+					screen[y * nScreenWidth + x] = nShade;
+				// Shade Floors
 				} else {
-					screen[y * nScreenWidth + x] = ' ';
+					float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
+					if (b < 0.25) nShade = '#';
+					else if (b < 0.5) nShade = 'x';
+					else if (b < 0.75) nShade = '.';
+					else if (b < 0.9) nShade = '-';
+					else nShade = ' ';
+					screen[y * nScreenWidth + x] = nShade;
 				}
 			}
 		}
